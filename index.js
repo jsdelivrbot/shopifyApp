d9600deb598d0306dde7940edbd8de1c2a3abc1a
Kevin Ling <lingkh107@gmail.com>
2017-07-10 16:16:30 +0100

    initial commit

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var route_install = function(request, response) {
+  res.sendfile('./public/install.js');
+ }
+app.get('/install', route_install);
+
+var route_finish_auth = function(request, response) { 
+    res.sendfile('./public/finish_auth.js');
+}
+app.get('/finish_auth', route_finish_auth);
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
