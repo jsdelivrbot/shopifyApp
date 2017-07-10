43da4f680a7ec8b1a2353e8292eca417aca9cb45
Kevin Ling <lingkh107@gmail.com>
2017-07-10 16:45:28 +0100

    Change base uri

---
+++ public/finish_auth.js
@@ -0,0 +1,38 @@
+var express = require('express');//requires Express
+var router = express.Router();//creates router object
+
+var shopifyAPI = require('shopify-node-api');//SHOPIFY
+
+//DEFINE LOCAL VARIABLES
+var shopify_hidden_at = "";//define empty variable for access token
+var ldb_uri_base = "https://nameless-ocean-79702.herokuapp.com/install";//THIS IS ONLY AN EXAMPLE!!
+var shopify_hidden_ss = "275648faae5fca94ba4950ee18fd2d66";
+var shopify_hidden_ak = "167922d6d9ace8e71795cb4c10074cf4";
+var shopify_hidden_shopname = "second-opinions-store";
+
+
+router.get('/', function(req, res){
+    //This is where it all happens: this is what happens you access "myn00bapp.herokuapp.com/install/"
+    
+    var query_params = req.query;
+    
+    //define the configs for your connection
+    var config = {
+        shop: shopify_hidden_shopname, // MYSHOP.myshopify.com
+        shopify_api_key: shopify_hidden_ak, // Your API key
+        shopify_shared_secret: shopify_hidden_ss, // Your Shared Secret
+        shopify_scope: 'write_products',//you might want to adapt the scope to your needs
+        redirect_uri: ldb_uri_base+'/finish_auth',//not sure whether you have to specify that, but hey it works
+        nonce: query_params.state//this is where you grab the random string created before
+    };
+    
+    //create new shopify object for the next step
+    var Shopify = new shopifyAPI(config);
+    
+    Shopify.exchange_temporary_token(query_params, function(err, data){
+        shopify_hidden_at=data['access_token'];
+        // YOU HAVE SAVED YOUR ACCESS_TOKEN YAY!
+    });
+});
+
+module.exports = router;
---
+++ public/install.js
@@ -0,0 +1,29 @@
+var express = require('express');//requires Express
+var router = express.Router();//creates router object
+var shopifyAPI = require('shopify-node-api');
+
+//DEFINE LOCAL VARIABLES
+var ldb_uri_base = "https://secondopinions-shopify.herokuapp.com/";//THIS IS ONLY AN EXAMPLE!!
+var shopify_hidden_ss = "275648faae5fca94ba4950ee18fd2d66";
+var shopify_hidden_ak = "167922d6d9ace8e71795cb4c10074cf4";
+var shopify_hidden_shopname = "second-opinions-store";
+var noncerequired = require('nonce')();
+var noncestring = noncerequired();
+
+//THIS ONE WRITES METAFIELDS
+// Shopify authentication process
+router.get('/', function(req, res, next) {
+  console.log("routing to install")
+  var Shopify = new shopifyAPI({
+    shop: shopify_hidden_shopname, // MYSHOP.myshopify.com
+    shopify_api_key: shopify_hidden_ak, // Your API key
+    shopify_shared_secret: shopify_hidden_ss, // Your Shared Secret
+    shopify_scope: 'write_products',// define the scope according to your needs!
+    redirect_uri: ldb_uri_base+'/finish_auth',
+    nonce: noncestring // you must provide a randomly selected value unique for each authorization request
+  });
+  var auth_url = Shopify.buildAuthURL();
+  res.redirect(auth_url);//this is where Shopify redirects you to ".../finish_auth/"!
+});
+
+module.exports = router;
