d7b93f278f54749af7e21b7b1cfa9e136e0cb1dd
User <user@users-mbp.home>
2017-08-15 12:58:00 +0100

    fix shopify api

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static(__dirname + '/public'))
 
 var route_install = require('./public/install');
 app.use('/install', route_install);
 
-//var route_finish_auth = require('./public/finish_auth');
-//app.use('/finish_auth', route_finish_auth);
-
-
-
-/*app.get('/', function(request, response) {
-  response.send('Hello World!')
-})*/
+var route_finish_auth = require('./public/finish_auth');
+app.use('/finish_auth', route_finish_auth);
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node2 app is running at localhost:" + app.get('port'))
 })
--- public/finish_auth.js
+++ public/finish_auth.js
@@ -1,17 +1,16 @@
 var express = require('express');//requires Express
-var router = express.Router();//creates router object
-
+var router = express.Router()
 var shopifyAPI = require('shopify-node-api');//SHOPIFY
 
 //DEFINE LOCAL VARIABLES
 var shopify_hidden_at = "";//define empty variable for access token
-var ldb_uri_base = "https://nameless-ocean-79702.herokuapp.com";//THIS IS ONLY AN EXAMPLE!!
+var ldb_uri_base = "http://localhost:5000";//THIS IS ONLY AN EXAMPLE!!
 var shopify_hidden_ss = "275648faae5fca94ba4950ee18fd2d66";
 var shopify_hidden_ak = "167922d6d9ace8e71795cb4c10074cf4";
 var shopify_hidden_shopname = "second-opinions-store";
 
 
-router.get('/finish_auth', function(req, res){
+router.get('/', function(req, res){
     //This is where it all happens: this is what happens you access "myn00bapp.herokuapp.com/install/"
     
     var query_params = req.query;
@@ -33,9 +32,28 @@ router.get('/finish_auth', function(req, res){
     
     Shopify.exchange_temporary_token(query_params, function(err, data){
         shopify_hidden_at=data['access_token'];
-        // YOU HAVE SAVED YOUR ACCESS_TOKEN YAY!
-        console.log("HELLO"+data)
+        console.log(shopify_hidden_at)  
     });
+    
+    var post_data = {
+        "product": {
+            "body_html": "<strong>Hi there!</strong>"
+        }
+    }
+    
+    /*var Shopify = new shopifyAPI({
+        shop: shopify_hidden_shopname,
+        shopify_api_key: shopify_hidden_ak,
+        shopify_shared_secret: shopify_hidden_ss,
+        access_token: shopify_hidden_at
+    })
+    
+    Shopify.post('/admin/products.json', post_data, function(err, response, headers){
+        console.log(response)
+    })*/
+    
+    res.sendFile(__dirname+'/shopify.html')
+    
 });
 
 module.exports = router;
