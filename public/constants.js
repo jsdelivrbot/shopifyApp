e93e8c635b180ca2d9aceeb1d796fa02d544deff
User <avi@secondopinions.help>
2017-09-02 15:11:14 +0100

    dfdfdffdf

--- public/constants.js
+++ public/constants.js
@@ -1,5 +1,5 @@
 var shopify_hidden_at = "";//define empty variable for access token
-var ldb_uri_base = "https://shopify-second-opinions-app.herokuapp.com";
+var ldb_uri_base = "http://localhost:5000";
 var shopify_hidden_ss = "2a2134b27760c013e1eebd784d5dc6fe";
 var shopify_hidden_ak = "167922d6d9ace8e71795cb4c10074cf4"
 
